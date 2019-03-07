0a6f607cdcba14b41805dd45615094c5920b5f15
Adam Christie <fractos@gmail.com>
2019-03-07 15:44:52 +0000

    add update after getting java installer. update name of log4j file in settings.

--- run_elucidate.sh
+++ run_elucidate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 aws s3 cp $S3_SETTINGS /opt/elucidate/elucidate.settings
-aws s3 cp $S3_LOG_SETTINGS /opt/elucidate/elucidate.log4j.settings.xml
+aws s3 cp $S3_LOG_SETTINGS /opt/elucidate/elucidate.log4j.properties
 aws s3 cp $S3_TOMCAT_LOG_SETTINGS /usr/local/tomcat/conf/logging.properties
 
 export JAVA_OPTS="-Delucidate.server.properties=file:/opt/elucidate/elucidate.settings -Xms${MIN_HEAP:-1024}m -Xmx${MAX_HEAP:-2048}m -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=512m"
